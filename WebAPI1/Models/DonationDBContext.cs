6dc93e39cb926c8e36a7d3bd7e77f539fcc36c58
Emanuel Martins Cardoso <emanuel.cardoso@t2mlab.com>
2024-03-14 16:34:22 -0300

    initial commit

---
+++ WebAPI1/Models/DonationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI1.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DCandidate> DCandidates { get; set; }
+
+    }
+}
